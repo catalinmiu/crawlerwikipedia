aa1fef418fccff29d9f9e40cb6c9ff6c25ab53d0
Catalin Miu <catalinmiu@192-168-0-221.rdsnet.ro>
2022-08-26 09:24:49 +0300

    add db backup

--- src/main/java/ro/pago/crawlerwikipedia/entity/City.java
+++ src/main/java/ro/pago/crawlerwikipedia/entity/City.java
@@ -18,11 +18,8 @@ public class City {
     @Id
     @GenericGenerator(name="CITY_ID_GENERATOR",
             strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
-            parameters = {
-                    @org.hibernate.annotations.Parameter(name = "sequence_name", value = "seq_cities"),
-                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "1"),
-                    @org.hibernate.annotations.Parameter(name = "increment_size", value = "1"),
-            })
+            parameters = {@org.hibernate.annotations.Parameter(name = "sequence_name", value = "seq_cities")})
+
     @GeneratedValue(generator="CITY_ID_GENERATOR")
     @Column(name = "city_id")
     private Integer cityId;
--- src/main/java/ro/pago/crawlerwikipedia/entity/Country.java
+++ src/main/java/ro/pago/crawlerwikipedia/entity/Country.java
@@ -17,11 +17,7 @@ public class Country {
     @Id
     @GenericGenerator(name="COUNTRY_ID_GENERATOR",
             strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
-            parameters = {
-                    @org.hibernate.annotations.Parameter(name = "sequence_name", value = "seq_countries"),
-                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "1"),
-                    @org.hibernate.annotations.Parameter(name = "increment_size", value = "1"),
-            })
+            parameters = {@org.hibernate.annotations.Parameter(name = "sequence_name", value = "seq_countries")})
     @GeneratedValue(generator="COUNTRY_ID_GENERATOR")
     @Column(name = "country_id")
     private Integer countryId;
